2e87496337e0f32c9b94fa662d02edd0a5677f80
yunjae62 <wodbs7758@naver.com>
2024-11-12 13:18:48 +0900

    refactor: 중복 API 제거

--- src/main/java/ex/mybatis/controller/ProductController.java
+++ src/main/java/ex/mybatis/controller/ProductController.java
@@ -11,29 +11,31 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductMapper productMapper;
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable Long id) {
         Product product = productMapper.getProduct(id);
         return ResponseEntity.ok(product);
     }
 
-    @PostMapping("/products")
+    @PostMapping("")
     public ResponseEntity<Product> insertProduct(@RequestBody ProductReq request) {
         Product product = request.toEntity();
         productMapper.insert(product);
         return ResponseEntity.ok(product);
     }
 
-    @PatchMapping("/products/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody ProductReq request) {
         Product product = productMapper.getProduct(id);
         product.update(request.toEntity());
@@ -41,7 +43,7 @@ public class ProductController {
         return ResponseEntity.ok(product);
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productMapper.delete(id);
         return ResponseEntity.ok().build();
