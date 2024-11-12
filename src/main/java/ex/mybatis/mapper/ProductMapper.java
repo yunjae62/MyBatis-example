74355bba9160f9eebd0a066b1994a84200fb68fa
yunjae62 <wodbs7758@naver.com>
2024-11-12 13:01:50 +0900

    feat: Product 매퍼에 생성 추가

--- src/main/java/ex/mybatis/mapper/ProductMapper.java
+++ src/main/java/ex/mybatis/mapper/ProductMapper.java
@@ -1,7 +1,9 @@
 package ex.mybatis.mapper;
 
 import ex.mybatis.domain.Product;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,4 +12,8 @@ public interface ProductMapper {
 
     @Select("SELECT * FROM PRODUCTS WHERE id = #{id}")
     Product getProduct(@Param("id") Long id);
+
+    @Insert("INSERT INTO PRODUCTS (name, price, stock_quantity) VALUES (#{name}, #{price}, #{stockQuantity})")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    void insert(Product product);
 }
