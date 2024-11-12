42917cfa2329ca79e6f9f3c06d89c8e28f4813fc
yunjae62 <wodbs7758@naver.com>
2024-11-12 11:37:01 +0900

    first commit

---
+++ src/test/java/ex/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,12 @@
+package ex.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
